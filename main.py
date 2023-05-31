a3740c44d638d8602af5637462966575d8b9f64e
KSV83UA <ksv83ua@gmail.com>
2023-05-31 09:37:03 +0300

    1

--- main.py
+++ main.py
@@ -4,6 +4,6 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('<h1>PyCharm</h1>')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
