c912e65a71290d416c2d1f5c9eb127b772b27baf
KSV83UA <ksv83ua@gmail.com>
2023-06-01 15:39:55 +0300

    first

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from setting.py import TOKEN
+
+
+
+
+# from setting.py import TOKEN
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
